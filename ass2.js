53092c75f774ac7cc42d58624fe19edde2e2e687
Kunal Das <92003108+Kd989@users.noreply.github.com>
2022-10-09 14:16:28 +0530

    practice accordian

---
+++ ass2.js
@@ -0,0 +1,30 @@
+function fun1(){
+    document.getElementById('main_div').style.display="block";
+}
+
+function fun2(){
+    document.getElementById('main_div').style.display="none";
+}
+
+
+function fun3(){
+    document.getElementById('table-1').setAttribute("border","2px");
+
+}
+
+function fun4(){
+    document.getElementById('acco-1').style.display = "block";
+    document.getElementById('acco-3').style.display = "none";
+    document.getElementById('acco-2').style.display = "none";
+
+}
+function fun5(){
+    document.getElementById('acco-2').style.display = "block";
+    document.getElementById('acco-1').style.display = "none";
+    document.getElementById('acco-3').style.display = "none";
+}
+function fun6(){
+    document.getElementById('acco-3').style.display = "block";
+    document.getElementById('acco-1').style.display = "none";
+    document.getElementById('acco-2').style.display = "none";
+}
